deeaa74f4c65fba6feb7cab75e92c5f17f29e7d9
katerina751 <katerina751@bk.ru>
2023-05-14 22:05:55 +0500

    Реализация единственного выбора уровня сложности

--- components/start-page-component.js
+++ components/start-page-component.js
@@ -1,34 +1,38 @@
 
-const levels = [
-    {
-        name: 1,
-        active: false,
-    },
-    {
-        name: 2,
-        active: false,
-    },
-    {
-        name: 3,
-        active: false,
-    },
-];
+// const levels = [
+//     {
+//         name: 1,
+//         active: false,
+//     },
+//     {
+//         name: 2,
+//         active: false,
+//     },
+//     {
+//         name: 3,
+//         active: false,
+//     },
+// ];
 
 
 export function renderStartPageComponent({ appEl }) {
-
-    const levelHtml = levels.map((level, index) => {
-        return `
-            <button data-index="${index}" id="level-button" class="${level.active ? 'window__level-button -active' : 'window__level-button'}">${level.name}</button>`;
-    })
-        .join("");
-
 
     const startWindowHtml = `
         <div class="window center">
             <div class="window__title">Выбери сложность</div>
             <div class="window__level">
-                ${levelHtml}
+                <div class="window__level-button">
+                    <input id="radio-1" type="radio" name="radio" value="1" checked>
+                    <label for="radio-1">1</label>
+                </div>
+                <div class="window__level-button">
+                    <input id="radio-2" type="radio" name="radio" value="2">
+                    <label for="radio-2">2</label>
+                </div>
+                <div class="window__level-button">
+                    <input id="radio-3" type="radio" name="radio" value="3">
+                    <label for="radio-3">3</label>
+                </div>
             </div>
             <div>
                 <button id="game__button" class="game__button">Старт</button>
@@ -37,87 +41,26 @@ export function renderStartPageComponent({ appEl }) {
 
     appEl.innerHTML = startWindowHtml;
 
-
     // Выбираем сложность уровня
 
+    function levelSelection() {
+        const buttonElement = document.getElementById("game__button")
 
-    const levelSelection = () => {
-        const buttonLevelElements = document.querySelectorAll(".window__level-button");
-
-        for (const buttonLevelElement of buttonLevelElements) {
-            buttonLevelElement.addEventListener("click", (event) => {
-                event.stopPropagation();
-                const index = buttonLevelElement.dataset.index;
-
-                if (levels[index].active === false) {
-                    levels[index].active = true;
-                } else if (levels[index].active === true) {
-                    levels[index].active = false;
+        buttonElement.addEventListener("click", () => {
+            function levels() {
+                let level = document.getElementsByName('radio');
+                for (var i = 0; i < level.length; i++) {
+                    if (level[i].checked) {
+                        console.log('Выбран ' + Number(i + 1) + ' уровень');
+                    }
                 }
-                renderStartPageComponent({ appEl });
-            });
-        }
-    };
+            }
 
-    // блокировка кнопки
-    // const validateInput = () => {
-    //     if (nameInputElement.value === "" || textInputElement.value === "") {
-    //         buttonElement.disabled = true;
-    //     } else {
-    //         buttonElement.disabled = false;
-    //     }
-    // };
-    // const buttonBlock = () => {
-    //     validateInput();
-    //     document.querySelectorAll("#name-input,#text-input").forEach((el) => {
-    //         el.addEventListener("input", () => {
-    //             validateInput();
-    //         });
-    //     });
-    // };
-
-    // buttonBlock();
-
-
-    // // Кнопка Старт
-    // const buttonElement = document.getElementById("game-button");
-
-    // buttonElement.addEventListener("click", () => {
-    //     // если не выбран уровень сложности, не пускаем дальше
-    //     if (addFormName.value === "" || addFormText.value === "") {
-    //         return;
-    //     }
-    //     // вводим переменную с необходимым формато даты
-    //     const options = {
-    //         year: '2-digit',
-    //         month: 'numeric',
-    //         day: 'numeric',
-    //         timezone: 'UTC',
-    //         hour: 'numeric',
-    //         minute: '2-digit'
-    //     };
-    //     let now = new Date().toLocaleString('ru-RU', options);
-
-    //     //   пополняем список после заполнения полей ввода
-
-    //     comments.push({
-    //         name: addFormName.value,
-    //         time: now,
-    //         text: addFormText.value,
-    //         counter: 0,
-    //         isLiked: false
-    //     });
-
-    //     initLikeButtons();
-    //     renderComments();
-    //     //   очищать поля ввода после добавления списка
-    //     addFormName.value = "";
-    //     addFormText.value = "";
-    // });
-
+            levels();
+        })
+    }
 
 
     levelSelection();
-
 
 };
